101afda0a183b1642709d2012076435f3f050438
Muteeb-Ur-Rehman-4 <muteeb.0004@gmail.com>
2024-06-29 18:24:41 +0500

    Minor fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,8 +9,10 @@ const AboutMe = () => {
       whileInView={{ opacity: 1 }} 
       className='flex flex-col items-center w-full justify-center p-4 '>
       <h1 className='text-5xl font-extrabold mt-4'>About me</h1>
-      <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col justify-around lg:items-start md:items-center sm:items-center mt-5 lg:w-[85vw] md:w-[55vw] sm:w-[60vw] ">
-        
+      <motion.div initial={{ opacity: 0 }}
+        transition={{duration:0.8}}
+        whileInView={{ opacity: 1 }}  
+        className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col justify-around lg:items-start md:items-center sm:items-center mt-5 lg:w-[85vw] md:w-[55vw] sm:w-[60vw] ">
         <div className='group flex flex-col justify-center w-full lg:w-[50vw] xs:w-[60vw] max-h-[20rem] m-10 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-3 transition-transform duration-300'>
           <div className='flex flex-col items-center '>
             <FontAwesomeIcon className='h-[6.75rem] lg:h-[6rem] md:h-[6.5rem] sm:h-[3.5rem] xs:h-[4.5rem] w-auto' icon={faComputer} />
@@ -21,7 +23,10 @@ const AboutMe = () => {
           </p>
         </div>
         
-        <div className='group flex flex-col justify-around w-full lg:w-[50vw] xs:w-[60vw] max-h-[20rem] m-10 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-3 transition-transform duration-300'>
+        <motion.div initial={{ opacity: 0 }}
+          transition={{duration:0.8}}
+          whileInView={{ opacity: 1 }} 
+          className='group flex flex-col justify-around w-full lg:w-[50vw] xs:w-[60vw] max-h-[20rem] m-10 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-3 transition-transform duration-300'>
           <div className='flex flex-col items-center'>
             <FontAwesomeIcon className='h-[6.75rem] lg:h-[6rem] md:h-[6.5rem] sm:h-[3.5rem] xs:h-[4.5rem] w-auto' icon={faGamepad} />
             <p className='text-center text-2xl md:text-xl font-bold mb-3'>Gaming enthusiast</p>
@@ -29,9 +34,12 @@ const AboutMe = () => {
           <p className='text-justify text-[1rem] opacity-0 max-h-0 group-hover:opacity-100 group-hover:max-h-60 transition-all duration-300'>
             I started gaming at a very young age and once you get into it you can never go back! From 2D sega games to Open world masterpieces like Elden ring is how I grew up....
           </p>
-        </div>
+        </motion.div>
         
-        <div className='group flex flex-col justify-around w-full lg:w-[50vw] xs:w-[60vw] max-h-[20rem] m-10 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-3 transition-transform duration-300'>
+        <motion.div initial={{ opacity: 0 }}
+          transition={{duration:0.8}}
+          whileInView={{ opacity: 1 }}  
+          className='group flex flex-col justify-around w-full lg:w-[50vw] xs:w-[60vw] max-h-[20rem] m-10 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-3 transition-transform duration-300'>
           <div className='flex flex-col items-center'>
             <FontAwesomeIcon className='h-[6.75rem] lg:h-[6rem] md:h-[6.5rem] sm:h-[3.5rem] xs:h-[4.5rem] w-auto' icon={faFutbol} />
             <p className='text-center text-2xl md:text-xl font-bold mb-3'>Football and Fitness</p>
@@ -39,9 +47,9 @@ const AboutMe = () => {
           <p className='text-justify text-[1rem] opacity-0 max-h-0 group-hover:opacity-100 group-hover:max-h-60 transition-all duration-300'>
             Growing up, I always loved playing football. Still trying to maintain a healthy physique because health is the most important thing in life!
           </p>
-        </div>
+        </motion.div>
       
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
